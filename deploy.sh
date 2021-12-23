d37a665e9c72f3e3fa6b606e417a6622af40ad19
yinhu <211026>
2021-12-23 09:48:37 +0800

    vue express dome init

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,22 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+npm run build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.gaoniao.top' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:yh-orange/vue-express-dome.git master:main
+# 如果你想要部署到 https://<USERNAME>.github.io
+git push -f git@github.com:yh-orange/yh-orange.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>  REPO=github上的项目
+# git push -f git@github.com:yh-orange/vue-express-dome.git master:gh-pages
 
 cd -
